6e099d378348444febcbd60481b3d3ae94420f93
Sasa Nedic <svetasmak0@gmail.com>
2025-11-02 17:28:38 +0100

    Fix

--- practicing-next.js/components/CalculateCounter.js
+++ practicing-next.js/components/CalculateCounter.js
@@ -7,7 +7,6 @@ const CalculateCounter = () => {
     <div>
       <h2 className="text-7xl font-bold mb-4">{count}</h2>
       <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
     </div>
   );
 };
