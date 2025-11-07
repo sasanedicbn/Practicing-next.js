13ef9cd5bf55fe2e79082036efe566377543e36f
Sasa Nedic <svetasmak0@gmail.com>
2025-11-07 15:25:51 +0100

    Create getAlltask function

--- practicing-next.js/app/prisma/page.js
+++ practicing-next.js/app/prisma/page.js
@@ -1,6 +1,14 @@
 import React from "react";
+import prisma from "@/utils/db";
 
-const Prisma = () => {
+const getAllTasks = async () => {
+  await prisma.task.create({
+    data: { content: "Do your math homework" },
+  });
+};
+
+const Prisma = async () => {
+  await getAllTasks();
   return <div>Prisma</div>;
 };
 
