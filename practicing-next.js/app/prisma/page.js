29a39579043e40589ab6923617466a3d297afd8c
Sasa Nedic <svetasmak0@gmail.com>
2025-11-07 15:37:05 +0100

    Display data

--- practicing-next.js/app/prisma/page.js
+++ practicing-next.js/app/prisma/page.js
@@ -5,11 +5,23 @@ const getAllTasks = async () => {
   await prisma.task.create({
     data: { content: "Do your math homework" },
   });
+
+  const tasks = await prisma.task.findMany();
+  return tasks;
 };
 
 const Prisma = async () => {
-  await getAllTasks();
-  return <div>Prisma</div>;
+  const allTasks = await getAllTasks();
+  return (
+    <div>
+      <h2 className="text-2xl text-amber-300">Prisma</h2>
+      <ul>
+        {allTasks?.map((task) => {
+          return <li key={task.id}>{task.content}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Prisma;
